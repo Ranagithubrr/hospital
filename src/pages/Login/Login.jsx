884aed7a0950c56cf64628640553cdcd978e9df8
ranagithubrr <mr6430119@gmail.com>
2023-09-08 06:07:02 +0600

    Sign Up and Sign In page updated

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SigninImg from '../../img/login.svg';
 
 const Login = () => {
     return (
-        <div className="w-screen h-screen flex bg-slate-100">
-            <div className="w-1/3 m-auto shadow-md rounded bg-white p-5">
-                <span className="text-3xl font-bold text-center block">Me<span className="text-blue-900">Doc</span></span>
-                <span className='block font-bold text-black text-center my-3 pb-2'>Log In</span>
-                <div className='w-3/5 m-auto py-2'>
-                    <input type="text" placeholder='Email (demo@gmail.com )' className='outline-none w-full  my-2 px-2 border-gray-400 border-2 rounded'/>
-                    <input type="password" placeholder='Input Your Password' className='outline-none w-full  my-2 px-2 border-gray-400 border-2 rounded'/>
-                    <span className='font-bold text-blue-900 text-sm cursor-pointer'>Forgot Password</span>
-                    <button className="bg-blue-700 text-white block rounded px-5 py-1 w-full my-4 hover:bg-blue-600">Log In</button>
-                    <span className='text-sm font-semibold text-gray-500'>Don't have an account ? <Link to="/register" className='font-bold text-blue-700 text-sm'>Sign Up</Link></span>
+        <div className="w-2/3 mx-auto h-screen items-center flex bg-white">
+            <div className="w-1/2">
+                <img src={SigninImg} alt="" />
+            </div>
+            <div className="w-1/2">
+                <div className='w-2/3 ml-10'>
+                    <h3 className='text-blue-500 font-sans font-bold text-xl mb-2'>Login Account</h3>
+                    <input type="text" placeholder='Email ID' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
+                    <input type="password" placeholder='Password' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
+                    <span className='text-sm font-semibold text-gray-500'> Not a Member ? <Link to="/register" className='text-blue-500'>Sign Up</Link></span>
+                    <button className='bg-blue-500 text-sm font-serif rounded-full block text-gray-100 w-full py-1 my-2'>Sign In</button>
                 </div>
             </div>
         </div>
--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import SigninImg from '../../img/signup.svg';
 
 const Register = () => {
     return (
-        <div className="w-screen h-screen flex bg-slate-100">
-            <div className="w-1/3 m-auto shadow-md rounded bg-white p-5">
-                <span className="text-3xl font-bold text-center block">Me<span className="text-blue-900">Doc</span></span>
-                <span className='block font-bold text-black text-center my-3 pb-2'>Register</span>
-                <div className='w-3/5 m-auto py-2'>
-                    <input type="text" placeholder='First Name' className='outline-none w-full  my-2 px-2 border-gray-400 border-2 rounded'/>
-                    <input type="text" placeholder='Last Name' className='outline-none w-full  my-2 px-2 border-gray-400 border-2 rounded'/>
-                    <input type="text" placeholder='Email (demo@gmail.com )' className='outline-none w-full  my-2 px-2 border-gray-400 border-2 rounded'/>
-                    <input type="password" placeholder='Input Your Password' className='outline-none w-full  my-2 px-2 border-gray-400 border-2 rounded'/>
-                    <input type="password" placeholder='Retype Password' className='outline-none w-full  my-2 px-2 border-gray-400 border-2 rounded'/>
-                    <span className='text-sm font-semibold'>I'm a </span>
-                    <input type="radio" name="userType" id="pataient" className="mx-2"/>
-                    <label htmlFor="pataient">Pataient</label>
-                    <input type="radio" name="userType" id="doctor" className='mx-2'/>
-                    <label htmlFor="doctor">Doctor</label>                   
-                    <br />                    
-                    <button className="bg-blue-700 text-white block rounded px-5 py-1 w-full my-4 hover:bg-blue-600">Log In</button>
-                    <span className='text-sm font-semibold text-gray-500'>Already have an account ? <Link to="/login" className='font-bold text-blue-700 text-sm'>Sign In</Link></span>
+        <div className="w-2/3 mx-auto h-screen items-center flex bg-white">
+            <div className="w-1/2">
+                <img src={SigninImg} alt="" className='rotat ' style={{ transform: 'scaleX(-1)' }} />
+            </div>
+            <div className="w-1/2">
+                <div className='w-2/3 ml-10'>
+                    <h3 className='text-blue-500 font-sans font-bold text-xl mb-2'>Create Account</h3>
+                    <input type="text" placeholder='Your Full Name' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
+                    <input type="text" placeholder='Email ID' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
+                    <input type="password" placeholder='Password' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
+                    <input type="password" placeholder='Confirm Password' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
+                    <div>
+                        <span className='text-sm font-semibold text-gray-500'>Gender : </span>
+                        <div className='flex items-center'>
+                            <input type="radio" name="sex" id="male" />
+                            <label htmlFor="male" className='text-sm font-semibold text-gray-500 px-2'>Male</label>
+                        </div>
+                        <div className='flex items-center'>
+                            <input type="radio" name="sex" id="famale" />
+                            <label htmlFor="famale" className='text-sm font-semibold text-gray-500 px-2'>Famale</label>
+                        </div>
+                    </div>
+                    <span className='text-sm font-semibold text-gray-500'> Already a Member ? <Link to="/login" className='text-blue-500'>Sign In</Link></span>
+                    <button className='bg-blue-500 text-sm font-serif rounded-full block text-gray-100 w-full py-1 my-2'>Sign Up</button>
                 </div>
             </div>
         </div>
